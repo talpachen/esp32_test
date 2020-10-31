f404bf30b0a774d930cc676354e831139a80090a
talpachen <talpachen@gmail.com>
2020-10-31 10:52:58 +0800

    [292/292] Generating binary image from built executable FAILED: .bin_timestamp cmd.exe /C "cd /D D:\projects\esp32_test\test_example\use_vsf_component_in_esp_idf\build && d:\Home\.espressif\python_env\idf4.2_py3.7_env\Scripts\python.exe D:/projects/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 elf2image --flash_mode dio --flash_freq 40m --flash_size 2MB --elf-sha256-offset 0xb0 -o D:/projects/esp32_test/test_example/use_vsf_component_in_esp_idf/build/hello-world.bin D:/projects/esp32_test/test_example/use_vsf_component_in_esp_idf/build/hello-world.elf && D:\Home\.espressif\tools\cmake\3.16.4\bin\cmake.exe -E echo "Generated D:/projects/esp32_test/test_example/use_vsf_component_in_esp_idf/build/hello-world.bin" && D:\Home\.espressif\tools\cmake\3.16.4\bin\cmake.exe -E md5sum D:/projects/esp32_test/test_example/use_vsf_component_in_esp_idf/build/hello-world.bin > D:/projects/esp32_test/test_example/use_vsf_component_in_esp_idf/build/.bin_timestamp" esptool.py v3.0-dev
    A fatal error occurred: Segment loaded at 0x400e6164 lands in same 64KB flash mapping as segment loaded at 0x400e6130. Can't generate binary. Suggest changing linker script or ELF
    to merge sections.
    ninja: build stopped: subcommand failed.

--- test_example/use_vsf_component_in_esp_idf/main/vsf_usr_cfg.h
+++ test_example/use_vsf_component_in_esp_idf/main/vsf_usr_cfg.h
@@ -28,6 +28,7 @@
 #define __RTOS__
 #define __FREERTOS__
 
+#define VSF_SYSTIMER_FREQ                               (0ul)
 
 #define VSF_ARCH_SWI_NUM                                4
 #define VSF_HAL_USE_USART                               DISABLED
@@ -41,107 +42,6 @@
 #   define VSF_HEAP_CFG_MCB_MAGIC_EN                    ENABLED
 #   define VSF_HEAP_SIZE                                0x1000
 #   define VSF_HEAP_CFG_MCB_ALIGN_BIT                   12      // 4K alignment
-
-// VSF_USE_USB_DEVICE will be enabled if target chip supports USBD
-//#define VSF_USE_USB_DEVICE                              ENABLED
-#   define VSF_USBD_CFG_USE_EDA                         ENABLED
-#   define VSF_USBD_USE_CDCACM                          ENABLED
-#   define VSF_USBD_USE_MSC                             ENABLED
-#   define VSF_USBD_USE_UVC                             ENABLED
-#   define APP_CFG_USBD_VID                             0xA7A8
-#   define APP_CFG_USBD_PID                             0x2348
-
-#define VSF_USE_MAL                                     DISABLED
-#   define VSF_MAL_USE_MEM_MAL                          ENABLED
-#   define VSF_MAL_USE_FAKEFAT32_MAL                    ENABLED
-#   define VSF_MAL_USE_SCSI_MAL                         ENABLED
-#   define VSF_MAL_USE_FILE_MAL                         ENABLED
-
-#define VSF_USE_SCSI                                    DISABLED
-#   define VSF_SCSI_USE_MAL_SCSI                        ENABLED
-
-#define VSF_USE_FS                                      DISABLED
-#   define VSF_FS_USE_MEMFS                             ENABLED
-#   define VSF_FS_USE_FATFS                             ENABLED
-
-#define VSF_USE_TRACE                                   DISABLED
-#define USRAPP_CFG_STDIO_EN                             ENABLED
-
-#define VSF_USE_LINUX                                   DISABLED
-#   define VSF_LINUX_USE_LIBUSB                         VSF_USE_USB_HOST
-#   define VSF_LINUX_USE_BUSYBOX                        ENABLED
-
-#ifndef USRAPP_CFG_LINUX_TTY_DEBUT_STREAM
-#   define USRAPP_CFG_LINUX_TTY_DEBUG_STREAM            0
-#   define USRAPP_CFG_LINUX_TTY_UART                    1
-#   define USRAPP_CFG_LINUX_TTY_CDC                     2
-#endif
-#   define USRAPP_CFG_LINUX_TTY                         USRAPP_CFG_LINUX_TTY_DEBUG_STREAM
-
-#define VSF_USE_STREAM                                  DISABLED
-#define VSF_USE_SIMPLE_STREAM                           ENABLED
-#define VSF_HAL_USE_DEBUG_STREAM                        ENABLED
-
-
-#ifndef USRAPP_CFG_DCD_TYPE_DEFAULT
-#   define USRAPP_CFG_DCD_TYPE_DEFAULT                  0
-#   define USRAPP_CFG_DCD_TYPE_DWCOTG                   1
-#   define USRAPP_CFG_DCD_TYPE_MUSB_FDRC                2
-#endif
-
-#define USRAPP_CFG_FAKEFAT32                            ENABLED
-
-
-#if __IS_COMPILER_LLVM__
-#   pragma clang diagnostic ignored "-Wbuiltin-requires-header"
-#   pragma clang diagnostic ignored "-Wmicrosoft-include"
-#   pragma clang diagnostic ignored "-Winconsistent-dllimport"
-#   pragma clang diagnostic ignored "-Wimplicit-function-declaration"
-#endif
-
-#define ASSERT(...)                                     assert(__VA_ARGS__)
-//#define ASSERT(...)
-
-#define VSF_SYSTIMER_FREQ                               (0ul)
-
-#define VSF_USE_USB_DEVICE                              DISABLED
-#   if VSF_ARCH_SWI_NUM == 0
-#       define VSF_USBD_CFG_EDA_PRIORITY                vsf_prio_0
-#   else
-#       define VSF_USBD_CFG_EDA_PRIORITY                vsf_prio_1
-#   endif
-#   if VSF_ARCH_PRI_NUM == 1
-#       define VSF_USBD_CFG_HW_PRIORITY                 vsf_arch_prio_0
-#   else
-#       define VSF_USBD_CFG_HW_PRIORITY                 vsf_arch_prio_1
-#   endif
-#   define USRAPP_CFG_USBD_DEV                          VSF_USB_DC0
-
-#define VSF_USBH_CFG_ENABLE_ROOT_HUB                    DISABLED
-#define VSF_USBH_USE_HUB                                DISABLED
-
-#if APP_USE_TGUI_DEMO == ENABLED
-#   define APP_CFG_TGUI_RES_DIR                         "./ui/"
-#endif
-
-#if APP_USE_XBOOT_XUI_DEMO == ENABLED
-#   define APP_CFG_XBOOT_RES_DIR                        "./ui/"
-#endif
-
-#if APP_USE_VSFIP_DEMO == ENABLED || APP_USE_LWIP_DEMO == ENABLED
-#   define VSF_NETDRV_USE_WPCAP                         ENABLED
-#       define VSF_NETDRV_WPCAP_CFG_HW_PRIORITY         vsf_arch_prio_0
-//  TODO: modify the virtual mac address
-#   define APP_NETDRV_WPCAP_CFG_MAC                     0xDC,0xFB,0x48,0x7B,0x9C,0x88
-#endif
-
-#if APP_USE_SDL2_DEMO == ENABLED
-#   define APP_CFG_SDL2_DEMO_COLOR_RGB565
-#endif
-
-#define VSF_LINUX_CFG_STACKSIZE                         (60 * 1024)
-#define VSF_TRACE_CFG_COLOR_EN                          ENABLED
-#define VSH_ECHO                                        1
 
 /*----------------------------------------------------------------------------*
  * Regarget Weak interface                                                    *
